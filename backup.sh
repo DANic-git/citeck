569c315a9fa10123ba49859a61dcff3b57cb3c5e
danic <mdan1@yandex.ru>
2015-10-29 15:47:56 +0600

    Бэкап базы с ошибкой

--- backup.sh
+++ backup.sh
@@ -17,18 +17,29 @@ function main
 if [[ $STATUS != 0 ]]; then
 	log "Произошла ошибка! Бэкап не удался"
 else	
+
 	log "Остановка tomcat"
 	/opt/alfresco-4.2.f/alfresco.sh stop tomcat
 	if [[ $? != 0 ]]; then
 		log "Ошибка при остановке tomcat"
 		exit
 	fi
+	
+	log "Копирование каталогов"
 	cd /opt/alfresco-4.2.f/
-	tar cfz /backup/files/$(date +"%y-%m-%d_%T").tar.gz ./alf_data
+	tar cfz /backup/files/$(date +"%y-%m-%d_%H-%M-%S").tar.gz --exclude='postgresql/*' ./alf_data
 	if [[ $? != 0 ]]; then
 		log "Ошибка при копировании каталогов"
 		exit
 	fi
+	
+	log "Выгрузка базы"
+	/opt/alfresco-4.2.f/postgresql/bin/pg_dump -w alfresco> /backup/sql/$(date +"%y-%m-%d_%H-%M-%S").sql
+	if [[ $? != 0 ]]; then
+		log "Ошибка при копировании бызы"
+		exit
+	fi
+	
 	log "Запуск tomcat"
 	/opt/alfresco-4.2.f/alfresco.sh start tomcat
 	if [[ $? != 0 ]]; then
