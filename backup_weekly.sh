cb6738d5fe10085f3fe33b266c8999061b6ee5f7
DANic <mdan1@yandex.ru>
2015-11-06 02:27:44 -0800

    add backup_weekly.sh

---
+++ backup_weekly.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+#Путь к файлу логов
+LOGFILE=/var/log/backup.log
+#Код с которым была завершена предыдущая команда. Если команда была выполнена удачно, то значение этой переменной будет 0, если же неудачно то не 0. 
+STATUS=$?
+#Первый парамт Backup Server второй Backup Patch
+BS=$1
+BP=$2
+
+#Директория скрипта
+DIRROOT=$(cd $(dirname $0) && pwd)
+
+#Вывод текущей даты и времени
+log(){
+   message="$(date +"%y-%m-%d %T") $@"
+   #echo $message
+   echo $message >>$LOGFILE
+}
+adddate() {
+    while IFS= read -r line; do
+        echo "$(date +"%y-%m-%d %T") $line"
+    done
+}
+
+#>$LOGFILE
+
+log "Начало бэкапа"
+function main
+{
+if [[ $STATUS != 0 ]]; then
+	log "Произошла ошибка! Бэкап не удался"
+else	
+
+	log "Остановка tomcat"
+	cd $DIRROOT
+	/opt/alfresco-4.2.f/alfresco.sh stop tomcat | adddate
+	if [[ $? != 0 ]]; then
+		log "Ошибка при остановке tomcat"
+		log "Запуск tomcat"
+			cd $DIRROOT
+			/opt/alfresco-4.2.f/alfresco.sh start tomcat | adddate
+			if [[ $? != 0 ]]; then
+				log "Ошибка при запуске tomcat"
+			fi		
+		exit
+	fi
+	
+	log "Копирование каталогов"
+	cd /opt/alfresco-4.2.f/
+	tar cfz /backup/weekly/files/$(date +"%y-%m-%d_%H-%M-%S").tar.gz --exclude='postgresql/*' ./alf_data | adddate
+	if [[ $? != 0 ]]; then
+		log "Ошибка при копировании каталогов"
+		log "Запуск tomcat"
+			cd $DIRROOT
+			/opt/alfresco-4.2.f/alfresco.sh start tomcat | adddate
+			if [[ $? != 0 ]]; then
+				log "Ошибка при запуске tomcat"
+			fi		
+		exit
+	fi
+	
+	log "Выгрузка базы"
+	/opt/alfresco-4.2.f/postgresql/bin/pg_dump -w -U alfresco alfresco> /backup/weekly/sql/$(date +"%y-%m-%d_%H-%M-%S").sql | adddate
+	if [[ $? != 0 ]]; then
+		log "Ошибка при копировании бызы"
+		log "Запуск tomcat"
+			cd $DIRROOT
+			/opt/alfresco-4.2.f/alfresco.sh start tomcat | adddate
+			if [[ $? != 0 ]]; then
+				log "Ошибка при запуске tomcat"
+			fi		
+		exit
+	fi
+	
+	log "Запуск tomcat"
+	cd $DIRROOT
+	/opt/alfresco-4.2.f/alfresco.sh start tomcat | adddate
+	if [[ $? != 0 ]]; then
+		log "Ошибка при запуске tomcat"
+		exit
+	fi	
+	
+	log "Копирование на удаленный сервер"
+	rsync -zvr /backup/weekly $BS::$BP | adddate
+	if [[ $? != 0 ]]; then
+		log "Ошибка при копировании на удаленный сервер"
+		exit
+	fi	
+	
+	log "Удаляем все бэкапы кроме последнего"
+	cd /backup/weekly/files/
+	if [[ $? != 0 ]]; then
+		log "Нет каталога /backup/weekly/files/"
+		exit
+	fi
+	rm -f `ls -t --full-time | awk '{if (NR > 2)printf("%s ",$9);}'`
+	cd /backup/weekly/sql/
+	if [[ $? != 0 ]]; then
+		log "Нет каталога /backup/weekly/files/"
+		exit
+	fi	
+	rm -f `ls -t --full-time | awk '{if (NR > 2)printf("%s ",$9);}'`
+	if [[ $? != 0 ]]; then
+		log "Ошибка при удалении бэкапов"
+		exit
+	fi
+fi	
+log "Успешное окончание Бэкапа"
+}
+
+main 2>&1 | tee -a $LOGFILE
+
+#Сокращаем лог файл
+tail -n 1000  $LOGFILE >/tmp/backup_log.tmp
+mv -f /tmp/backup_log.tmp $LOGFILE
